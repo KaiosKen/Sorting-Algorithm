33f8146b08547496277917faac892510e169a28d
Robel Abraham <robel.abraham@student.allenisd.org>
2022-09-13 14:04:17 -0500

    Made the beggining of the Sorting

--- Sorting.swift
+++ Sorting.swift
@@ -1,10 +1,20 @@
 import Foundation
 
-var word : String
+var randomWords : Array<String>
+randomWords = ["Pink", "Burger", "Yeti", "Xylophone", "Apple", "Oreo", "England"]
 var sequence = 0
-
-func WordtoNumber(word : String, sequence: Int) {
-
-    var value = Array(word)[0]
+var wordCount = randomWords.count
+func Sorter(words : Array<String>, sequence: Int, wordCount: Int) {
+    for sequence in 0...wordCount {
+        var word1 : UInt8 = 1
+        word1 = (words[sequence].first!).asciiValue!
+        print(word1)
+        if word1 != 1 {
+            print("Success")
+        }
+    }
+   
 
 }
+Sorter(words: randomWords, sequence:sequence, wordCount:wordCount)  
+//WordtoNumber(randomWords[sequence]: word
